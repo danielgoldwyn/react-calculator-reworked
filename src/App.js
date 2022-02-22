1de3f4b6c8c660d45f0f175a1ad323d21e394532
Daniel Goldwyn <danielgoldwyn@hotmail.com>
2022-02-22 21:53:48 +0800

    first init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const operatorButtons = ["/", "*", "-", "+", "."];
+
+  function parse(str) {
+    let x = Function(`'use strict'; return (${str})`)();
+    return x.toString();
+  }
+
+  function updateCalc(value) {
+    if (operatorButtons.includes(value)) {
+      if (calc === "" || operatorButtons.includes(calc.slice(-1))) {
+        return;
+      }
+    }
+
+    let currentCalc = calc;
+    currentCalc = currentCalc + value;
+
+    setCalc(currentCalc);
+
+    if (!operatorButtons.includes(currentCalc.slice(-1))) {
+      setResult(parse(currentCalc));
+    }
+  }
+
+  const createNumbers = () => {
+    var num = [];
+    for (let x = 1; x < 10; x++) {
+      num.push(
+        <button key={x} onClick={() => updateCalc(x.toString())}>
+          {x}
+        </button>
+      );
+    }
+    return num;
+  };
+
+  const calculate = () => {
+    if (operatorButtons.includes(calc.slice(-1))) {
+      return;
+    }
+    setCalc(parse(calc));
+  };
+
+  const deleteLast = () => {
+    let currentCalc = calc;
+
+    if (currentCalc === "") {
+      return;
+    } else if (currentCalc.length === 1) {
+      clearCalc();
+      return;
+    }
+
+    let value = currentCalc.slice(0, -1);
+    setCalc(value);
+
+    if (operatorButtons.includes(value.slice(-1))) {
+      setResult(parse(value.slice(0, -1)));
+    } else {
+      setResult(parse(value));
+    }
+  };
+
+  const clearCalc = () => {
+    setCalc("");
+    setResult("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="display">
+          {result ? <span>({result})</span> : ""} {calc || 0}
+        </div>
+
+        <div className="operators">
+          <button onClick={() => updateCalc("/")}>/</button>
+          <button onClick={() => updateCalc("*")}>*</button>
+          <button onClick={() => updateCalc("-")}>-</button>
+          <button onClick={() => updateCalc("+")}>+</button>
+
+          <button onClick={deleteLast}>DEL</button>
+          <button onClick={clearCalc}>C</button>
+        </div>
+
+        <div className="numbers">
+          {createNumbers()}
+          <button onClick={() => updateCalc("0")}>0</button>
+          <button onClick={() => updateCalc(".")}>.</button>
+          <button onClick={calculate}>=</button>
+        </div>
+      </div>
     </div>
   );
 }
